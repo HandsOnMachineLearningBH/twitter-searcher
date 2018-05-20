fc88bb0646c055fcc7d33c2db21984c07e42d219
Adriano Godoy <agodoy@thoughtworks.com>
2018-05-19 22:05:18 -0300

    Build row object to be used for analysis

--- index.js
+++ index.js
@@ -7,29 +7,67 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-function getTweets(term, numberOfResults, params={q: term, include_entities: true, lang: 'pt', count: 10}) {
 
+async function searchByTweets(term, numberOfResults, results = [], params={include_entities: true, lang: 'pt', count: 10}) {
   if (numberOfResults > 0) {
-    // https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets.html
-    client.get('search/tweets', params, function(error, tweets, response) {
-      if (error) { console.error(error); }
 
+    params.q = term;
+    const tweets = await client.get('search/tweets', params);
+
+    if (tweets) {
       const statuses = tweets.statuses;
       const metadata = tweets.search_metadata;
 
       if (metadata.count > 0) {
-        console.log(tweets.statuses[2]);
-        console.log(metadata);
+        for(let tweet of tweets.statuses) {
+          const rowData = buildRowData(tweet);
+          results.push(rowData);
+        }
 
-        numberOfResults -= tweets.statuses.length;
-
+        numberOfResults -= statuses.length;
         params.max_id = metadata.max_id;
-        getTweets(term, numberOfResults, params);
+
+        return await searchByTweets(term, numberOfResults, results, params);
+      } else {
+        return results;
       }
-
-
-    });
+    }
+  } else {
+    return results;
   }
 }
+
+function buildRowData(tweet) {
+  let rowData = {};
+  rowData.text = tweet.text;
+  rowData.truncated = tweet.truncated;
+  rowData.retweetCount = tweet.retweet_count;
+  rowData.favoriteCount = tweet.favorite_count;
+  rowData.lang = tweet.lang;
+
+  const entities = tweet.entities;
+  rowData.hashtagsCount = entities.hashtags.length;
+  rowData.mentionsCount = entities.user_mentions.length;
+  rowData.urlsCount = entities.urls.length;
+
+  const user = tweet.user;
+  rowData.userScreenName = user.screen_name;
+  rowData.userName = user.name;
+  rowData.userLocation = user.location;
+  rowData.userFollowers = user.followers_count;
+  rowData.userFriends = user.friends_count;
+  rowData.userFavorites = user.favourites_count;
+  rowData.userStatusesCount = user.statuses_count;
+
+  return rowData;
+}
+
+async function getTweets(term, maxResults) {
+  const results = await searchByTweets(term, maxResults);
+  console.log("final result", results);
+
+}
+
+
 
 getTweets('Latam', 20);
